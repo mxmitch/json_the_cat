79de7f287fe1e85cf052934cca6cd0bb69fd7e9e
Mitch Lum <mklum88@gmail.com>
2019-12-06 02:25:36 +0000

    callback function implemented to get info from API request

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,13 +2,18 @@ const request = require('request');
 
 const API = "https://api.thecatapi.com/v1/breeds/search?q=" + process.argv[2];
 
-request(API, (error, response, body) => {
-  if (response) {
+const fetchBreedDescription = function(breedName, callback) {
+  request(API, (error, response, body) => {
     const data = JSON.parse(body);
-    if (body === "[]") {
-      console.log("Error: Please enter a valid breed");
+    if (data.length !== 0) {
+      desc = data[0].description;
+    } else {
+      desc = "This is an error message";
     }
-    console.log(data[0]['description']);
-  }
-  console.log(error);
-});
+    callback(error, desc);
+  });
+};
+
+module.exports = {
+  fetchBreedDescription
+};
